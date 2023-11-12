26c0a6b536e874875227d7c2bc2d8f1d3d7dc33e
mariavjs <mariavjs@al.insper.edu.br>
2023-11-11 22:54:11 -0300

    FUNCIONOU

--- agenda-frontend/src/App.jsx
+++ agenda-frontend/src/App.jsx
@@ -1,115 +1,62 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Formulario from "./components/Formulario"; 
-//import "./App.css";
+import "./App.css";
 
 
-async function getToken() {
-  const apiEndpoint = 'https://api.mailpro.com/v3/token';
-
-  const requestData = new URLSearchParams();
-  
-  requestData.append('grant_type', 'password');
-  
-  requestData.append('username', 'mariavjs@al.insper.edu.br'); // Substitua com o seu nome de usuário
-  
-  requestData.append('password', '914af416-db21-4581-9a3f-dcd92b121676'); // Substitua com a sua senha da API
-  
-  const options = {
-  
-      headers: {
-      
-      'accept': 'application/json',
-      
-      'Content-Type': 'application/x-www-form-urlencoded', 
-      
-      },
-      
-      }
-    
-
-    const response = await axios.post(apiEndpoint, requestData, options);
-
-    const token = response.data.access_token;
-  
-    return token;
-
-    
-}
-//getToken();
-
-// Fazendo a requisição do email
-async function sendMail(user, name) {
-    try {
-        const bearerToken = await getToken();
-        
-        const email = {
-            email: user,
-            id_sender_email: 322291,
-            body_html: `Olá ${name}, bem vinde a nossa familia! Entre no site para marcar sua primeira aula`,
-            date_planned: null,
-            subject: 'Bem Vinde!!',
-        };
-
-            const headers = {
-
-                'Authorization': `Bearer ${bearerToken}`,
-                
-                'Content-Type': 'application/json'
-                
-                };
-                
-                
-            const config = {
-                
-                method: 'POST',
-                
-                url: "https://api.mailpro.com/v3/send/mail",
-                
-                headers: headers,
-                
-                data: JSON.stringify(email)
-                
-                };
-                
-            axios(config).then((response) => console.log(response.data));
-
-    } catch (error) {
-        console.error(error);
-    }
-}
 
 function App() {
-  const [contatos, setContatos] = useState([]);
+  //const [contatos, setContatos] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-  const carregaContatos = () => {
+
+  // const carregaContatos = () => {
+  //   axios
+  //   .get("http://localhost:8000/api/emailsuser/")
+  //   .then((res) => setContatos(res.data));
+  // }
+
+  // useEffect(() => {
+  //   carregaContatos();
+
+  // }, []);
+
+  const criarContato = (event) => {
+    event.preventDefault();
+
+    var data = {
+        "name" : name,
+        "email": email
+    }
+
     axios
-    .get("http://localhost:8000/api/emailsuser/")
-    .then((res) => setContatos(res.data));
-  }
+        .post("http://localhost:8000/api/emailsuser/", data=data)
+        .then(() => {
+            props.funcao();
+            setName("");
+            setEmail("");
+        })
 
-  useEffect(() => {
-    carregaContatos();
+}
 
-  }, []);
-
-  console.log(contatos);
+ // console.log(contatos);
 
   return (
     <div className="App">
 
-      <head>
-        <meta charset="UTF-8" />
+      <div>
+        <meta charSet="UTF-8" />
         <title>YOGA</title>
-      </head>
+      </div>
 
-      <body>
+      <div>
         <div className="appbar">
         <img src='/logo-yoga.png' />
         </div>
-      </body>
+      </div>
     
-      <Formulario  funcao={carregaContatos}/>
+      <Formulario  funcao={criarContato}/>
 
     </div>
   );
--- agenda-frontend/src/components/Formulario/index.jsx
+++ agenda-frontend/src/components/Formulario/index.jsx
@@ -1,45 +1,105 @@
+// import axios from "axios";
+// import React, { useState } from "react";
+
+
+
+// export default function Formulario(props) {
+
+//     const [name, setName] = useState("");
+//     const [email, setEmail] = useState("");
+
+//     const criarContato = (event) => {
+
+        
+//         event.preventDefault();
+    
+//         const data = {
+//                 name: name,
+//                 email: email
+//             };
+    
+//             axios.post("http://localhost:8000/api/emailsuser/", data, {
+
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 }
+//             })
+//             .then(() => {
+//                 setName("");
+//                 setEmail("");
+                
+//             })
+//             .catch(error => {
+//                 console.error("Erro ao enviar formulário:", error);
+//             });
+
+
+//     }
+
+//     return (
+//         <form className="form-card" method="post" onSubmit={criarContato}>
+//             <input
+//                 className="form-card-title"
+//                 type="text"
+//                 name="name"
+//                 placeholder="Name"
+//                 onChange={ (event)=>{setName(event.target.value)} }
+//                 value={name}
+//             />
+//             <textarea
+//                 className="autoresize"
+//                 name="email"
+//                 placeholder="Email"
+//                 onChange={ (event)=>{setEmail(event.target.value)} }
+//                 value={email}
+//             ></textarea>
+//             <button className="btn" type="submit">Send</button>
+//         </form>
+//     );
+// }
+
 import axios from "axios";
 import React, { useState } from "react";
 
-export default function Formulario(props) {
-
+export default function Formulario() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
 
     const criarContato = (event) => {
         event.preventDefault();
+        const data = { name, email };
 
-        const data = {
-            "name" : name,
-            "email": email
-        }
-
-        axios
-            .post("http://localhost:8000/api/emailsuser/", data)
-            .then(() => {
-                props.funcao();
-                setName("");
-                setEmail("");
-            })
-
-        sendMail(email, name);
-    }
+        axios.post("http://localhost:8000/api/emailsuser/", data, {
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(() => {
+            setName("");
+            setEmail("");
+            alert("Contato criado com sucesso!");
+        })
+        .catch(error => {
+            if (error.response && error.response.data) {
+                alert(error.response.data); // Mostra a mensagem do backend
+            } else {
+                console.error("Erro ao enviar formulário:", error);
+                alert("Erro ao enviar formulário.");
+            }
+        });
+    };
 
     return (
-        <form className="form-card" method="post" onSubmit={criarContato}>
+        <form className="form-card" onSubmit={criarContato}>
             <input
                 className="form-card-title"
                 type="text"
-                name="name"
                 placeholder="Name"
-                onChange={ (event)=>{setName(event.target.value)} }
+                onChange={e => setName(e.target.value)}
                 value={name}
             />
             <textarea
                 className="autoresize"
-                name="email"
                 placeholder="Email"
-                onChange={ (event)=>{setEmail(event.target.value)} }
+                onChange={e => setEmail(e.target.value)}
                 value={email}
             ></textarea>
             <button className="btn" type="submit">Send</button>
