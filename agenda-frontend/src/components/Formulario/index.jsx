877ac3af88e7173088e5f7214055cc7d8ffcadcb
mariavjs <mariavjs@al.insper.edu.br>
2023-11-11 23:09:07 -0300

    alguns ajustes no css

--- agenda-frontend/src/components/Formulario/index.jsx
+++ agenda-frontend/src/components/Formulario/index.jsx
@@ -1,65 +1,6 @@
-// import axios from "axios";
-// import React, { useState } from "react";
-
-
-
-// export default function Formulario(props) {
-
-//     const [name, setName] = useState("");
-//     const [email, setEmail] = useState("");
-
-//     const criarContato = (event) => {
-
-        
-//         event.preventDefault();
-    
-//         const data = {
-//                 name: name,
-//                 email: email
-//             };
-    
-//             axios.post("http://localhost:8000/api/emailsuser/", data, {
-
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 }
-//             })
-//             .then(() => {
-//                 setName("");
-//                 setEmail("");
-                
-//             })
-//             .catch(error => {
-//                 console.error("Erro ao enviar formulário:", error);
-//             });
-
-
-//     }
-
-//     return (
-//         <form className="form-card" method="post" onSubmit={criarContato}>
-//             <input
-//                 className="form-card-title"
-//                 type="text"
-//                 name="name"
-//                 placeholder="Name"
-//                 onChange={ (event)=>{setName(event.target.value)} }
-//                 value={name}
-//             />
-//             <textarea
-//                 className="autoresize"
-//                 name="email"
-//                 placeholder="Email"
-//                 onChange={ (event)=>{setEmail(event.target.value)} }
-//                 value={email}
-//             ></textarea>
-//             <button className="btn" type="submit">Send</button>
-//         </form>
-//     );
-// }
-
 import axios from "axios";
 import React, { useState } from "react";
+import "./index.css";
 
 export default function Formulario() {
     const [name, setName] = useState("");
